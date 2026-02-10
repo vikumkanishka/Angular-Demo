8e8040c387f1d92a50fbca449fc3f93146b8c906
Vikum Kanishka <vikumkanishka14@gmail.com>
2026-02-10 14:45:20 +0530

    Refactor customer data handling with two-way data binding and update template for dynamic display

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,6 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { Nav } from "./page/nav/nav";
 import { FormsModule } from '@angular/forms';
 
+interface Customer{
+  name: String;
+  age:Number;
+  address:String; 
+}
+
 @Component({
   selector: 'app-root',
   imports: [Nav, FormsModule],
@@ -10,9 +16,18 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './app.css'
 })
 export class App {
+
+  customer : Customer = {
+    name:'',
+    age :0,
+    address :''
+  }
+  
   name: string = 'Angular';
 
   clickMe(){
     alert("button clicked");
-  }   
+  }
+  
+  
 }
